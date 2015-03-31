1222daf5b3b2cb70e8532200026b22ecf52ef194
Iulia Soimaru <yuliga4@gmail.com>
2015-03-30 20:50:42 -0700

    fix ajax call, now user can search movie by one

--- public/js/application.js
+++ public/js/application.js
@@ -12,15 +12,40 @@ $(document).ready(function() {
       url: "http://www.omdbapi.com/?t=" + $('.title').val() + "&y=&plot=long"
       // data:
     }).done(function(data){
-      $('.search_result .image').append('<img class="poster" src="' + data.Poster + '">')
-      $('.search_result .movie_description').append('<p>Title: ' + data.Title + '</p>')
-      $('.search_result .movie_description').append('<p>Year: ' + data.Year + '</p>')
-      $('.search_result .movie_description').append('<p>Duration: ' + data.Runtime + '</p>')
-      $('.search_result .movie_description').append('<p>Actors: ' + data.Actors + '</p>')
-      $('.search_result .movie_description').append('<p>Rating: ' + data.imdbRating + '</p>')
+      $('.search_result').css({display: "block"})
+      var $post_template = $('.post-template');
+      $post_template.find('.image img').attr('src', data.Poster);
+      $post_template.find('.title span').text(data.Title);
+      $post_template.find('.year span').text(data.Year);
+      $post_template.find('.duration span').text(data.Runtime);
+      $post_template.find('.actors span').text(data.Actors);
+      $post_template.find('.director span').text(data.Director);
+      $post_template.find('.rating span').text(data.imdbRating);
+
+
+      // $('.search_result').append("<div class='post'>");
+      // $('.post').append("<div class='image'>");
+      // $('.post .image').append('<img class="poster" src="' + data.Poster + '">')
+      // $('.post').append('</div>');
+
+      // $('.post').append('<div class="movie_description">');
+      // $('.post .movie_description').append('<p>Title: ' + data.Title + '</p>');
+      // $('.post .movie_description').append('<p>Year: ' + data.Year + '</p>');
+      // $('.post .movie_description').append('<p>Duration: ' + data.Runtime + '</p>');
+      // $('.post .movie_description').append('<p>Actors: ' + data.Actors + '</p>');
+      // $('.post .movie_description').append('<p>Rating: ' + data.imdbRating + '</p>');
+      // $('.post').append('</div>');
+
+      $('.search_result').append($post_template);
+      $('.title').val('')
 
     }).fail(function(){
       console.log('call failed!')
-    })
-  })
+    });
+  });
+
+
+///////////////////////////////////////////////////////////////////
+
+  // $('.')
 });
